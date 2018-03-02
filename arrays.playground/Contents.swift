2a2858113cfb6308f16dc61c434f307b4c18a625
West Kraemer <kraemerwest@gmail.com>
2018-03-01 20:38:06 -0500

    add array basics

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//array, ordered list of values. Can only store values of the same type.
+
+var evenNumbers: [Int] = [2, 4, 6, 8]
+
+let evenNumbers2: Array<Int> = [12, 14, 16, 18]
+
+evenNumbers.append(10)
+
+evenNumbers += [20, 22, 26]
+
+print(evenNumbers.isEmpty)
+
+print(evenNumbers.count)
+
+//because this is an optional you need to check to see if it's nil or not
+if let goob = evenNumbers.first {
+print(goob)
+} else {
+    print("emp")
+}
+
+

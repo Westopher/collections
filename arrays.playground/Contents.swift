5e34f4698ddb9fc6fd647d472e40d75a33fb73c4
West Kraemer <kraemerwest@gmail.com>
2018-03-01 20:44:29 -0500

    use ranges, insert, min, etc... in arrays

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 //array, ordered list of values. Can only store values of the same type.
 
-var evenNumbers: [Int] = [2, 4, 6, 8]
+var evenNumbers: [Int] = [2, 4, 6, 8, 0]
 
 let evenNumbers2: Array<Int> = [12, 14, 16, 18]
 
@@ -17,10 +17,28 @@ print(evenNumbers.isEmpty)
 print(evenNumbers.count)
 
 //because this is an optional you need to check to see if it's nil or not
+//evenNumbers = []
+
 if let goob = evenNumbers.first {
 print(goob)
 } else {
     print("emp")
 }
+
+//print the smallest # in the array
+if let min = evenNumbers.min() {
+    print(min)
+}
+
+var firstElement = evenNumbers[0]
+print("first element is \(firstElement)")
+
+let firstThree = evenNumbers[0...2]
+print(firstThree)
+
+evenNumbers.contains(3)
+
+evenNumbers.insert(62, at: 4)
+print(evenNumbers)
 
 

a396ccca6c2ff4d4a02ecd88711ee2f4dc63403a
West Kraemer <kraemerwest@gmail.com>
2018-03-04 14:52:34 -0500

    adding the results of a dictoonary

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -60,6 +60,54 @@ counter1()
 counter2()
 
 
+//closures and collections
+
+let names = ["Zeplin", "Banford", "Apple", "Cutford"]
+
+names.sorted { a, b in
+    a < b
+}
+
+var prices = [1.50, 10.00, 4.99, 2.30, 8.19]
+
+let largePrices = prices.filter { price -> Bool in
+    return price > 5
+}
+
+let salePrices = prices.map { price -> Double in
+    return price * 0.9
+}
+print(salePrices)
+
+let sum = prices.reduce(0) { result, price -> Double in
+    return result + price
+}
+
+//to find the value of a dictionary where the key represents the price and the value represents the number of items in stock at that price
+var stock = [1.50: 5, 10.00: 2, 4.99: 20, 2.30: 5, 8.19: 30]
+let stockSum = stock.reduce(0) { result, pair -> Double in
+    return result + (pair.key * Double(pair.value))
+}
+
+print(stockSum) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

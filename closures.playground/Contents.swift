64784c236a63ba57176927000d5640b544d13810
West Kraemer <kraemerwest@gmail.com>
2018-03-04 12:06:08 -0500

    add closures re: functions

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -2,4 +2,29 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//closures
+
+var multiplyClosure: (Int, Int) -> Int
+
+multiplyClosure = { a, b  in
+    return a * b
+}
+
+let result = multiplyClosure(4, 2)
+
+//functions take closures as a parameter
+
+func operateOnNumbers(_ a: Int, _ b: Int,
+                      operation: (Int, Int) -> Int) -> Int {
+    let result = operation(a, b)
+    print(result)
+    return result
+}
+//take these parameters and do this function to it
+operateOnNumbers(2, 4, operation: multiplyClosure)
+
+
+
+
+
+
